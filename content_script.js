201abf5ce480c766c2a9dbd236615171ae51beee
David Burger <davidjburger@yahoo.com>
2010-01-11 22:20:18 -0800

    work on the overlay alarm

--- content_script.js
+++ content_script.js
@@ -1,21 +1,31 @@
-var div = document.createElement("div");
-div.style.position = "fixed";
-div.style.left = div.style.top = 0;
-div.style.width = div.style.height = "100%";
-div.style.background = "white";
-div.style.opacity = 0.9;
+var overlay = document.createElement("div");
+overlay.style.position = "fixed";
+overlay.style.left = overlay.style.top = 0;
+overlay.style.width = overlay.style.height = "100%";
+overlay.style.background = "white";
+overlay.style.opacity = 0.9;
+
+var badge = document.createElement("div");
+badge.style.position = "fixed";
+badge.style.width = badge.style.height = "100px";
+badge.style.left = badge.style.top = "50%";
+badge.style.marginLeft = badge.style.marginTop = "-50px";
+badge.style.background = "yellow";
+badge.innerHTML = "OK, Back to Work!";
 
 var addOverlay = function() {
-    document.body.appendChild(div);
+    document.body.appendChild(overlay);
+    document.body.appendChild(badge);
 };
 
 var removeOverlay = function() {
-    document.body.removeChild(div);
+    document.body.removeChild(overlay);
+    document.body.removeChild(badge);
 };
 
 chrome.extension.onRequest.addListener(function(msg, sender, callback) {
     if (msg.op === "add") {
-        div.onclick = function () {
+        badge.onclick = function () {
             callback();
         };
         addOverlay();
